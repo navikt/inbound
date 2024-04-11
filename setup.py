d77dacf525d6f9241319060c26d682fc3ec5aad7
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-04-11 10:49:24 +0200

    Fikse feil referering til dotenv i setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=["oracledb>=2.0.0", "pyodbc", "jinja2", "dotenv"],
+    install_requires=["oracledb>=2.0.0", "pyodbc", "jinja2", "python-dotenv"],
     python_requires=">=3.11",
 )
