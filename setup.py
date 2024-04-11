dd5ed1406ccfd83a71963508cbe2bc22fd7c0959
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-04-11 10:58:22 +0200

    Legge til dataclass i avhengigheter

--- setup.py
+++ setup.py
@@ -22,6 +22,12 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=["oracledb>=2.0.0", "pyodbc", "jinja2", "python-dotenv>=1.0.0"],
+    install_requires=[
+        "oracledb>=2.0.0",
+        "pyodbc",
+        "jinja2",
+        "python-dotenv>=1.0.0",
+        "dataclass",
+    ],
     python_requires=">=3.11",
 )
