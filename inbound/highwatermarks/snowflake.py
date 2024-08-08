9b0b11624df4d00a6d51fe56cec0f761cc7642d2
Patrick Pedersen <patrick.pedersen@nav.no>
2024-08-08 10:48:43 +0200

    Sjekke om nødvendige parametere er fylt ut i SnowHighwatermark

--- inbound/highwatermarks/snowflake.py
+++ inbound/highwatermarks/snowflake.py
@@ -5,7 +5,9 @@ from inbound.sdk.highwatermark import Highwatermark
 
 class SnowHighwatermark(Highwatermark):
     def __init__(self, connection: SnowflakeConnection, query: str):
+        assert connection is not None, "Connection is None"
         self.connection = connection
+        assert query is not None, "Query is None"
         self.query = query
 
     def generate_query_list(self) -> list[dict]:
