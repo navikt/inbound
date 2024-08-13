468ebf3a178509b9292b798f7108d43022c4c1c1
trineno <161321609+trineno@users.noreply.github.com>
2024-08-13 18:11:17 +0200

    Utvide MainManagerTap integration test
    Samt fjerne os.environ fra tap, og liten endring på pip install i makefile

--- inbound/taps/mainmanager.py
+++ inbound/taps/mainmanager.py
@@ -1,23 +1,34 @@
 import json
-import os
-import requests
 from typing import Any, Generator
 from urllib.parse import urljoin
 
-from ..sdk.tap import Tap
+import requests
+
 from ..core.models import Description
+from ..sdk.tap import Tap
+
 
 class MainManagerTap(Tap):
 
-    def __init__(self, table: str, username: str, password: str):
+    def __init__(
+        self,
+        table: str,
+        username: str,
+        password: str,
+        base_url: str = "https://nav-test.mainmanager.no",
+    ):
         self.table = table
         self.username = username
         self.password = password
-        self.base_url = os.environ["MAINMANAGER_URL"]
+        self.base_url = base_url
         self.endpoint = f"/api/v1/datawarehouseview?viewname={table}&datatableoutput=4)"
 
     def column_descriptions(self) -> list[Description]:
-        return [Description(name="raw", type="variant", precision=None, scale=None, nullable=True)]
+        return [
+            Description(
+                name="raw", type="variant", precision=None, scale=None, nullable=True
+            )
+        ]
 
     # Henter ut json-data fra APIet og deler opp i én record per rad
     def data_generator(self) -> Generator[list[tuple], Any, None]:
@@ -31,22 +42,22 @@ class MainManagerTap(Tap):
         # TODO: vurdere om informasjon om antall rader skal være med til snowflake
         # print('Antall records hentet fra MainManager-API: ', str(eiendomsdata['TotalNumberOfRecords']))
 
-        table_content = json.loads(eiendomsdata.get('DataTable'))
+        table_content = json.loads(eiendomsdata.get("DataTable"))
 
-        return([(json.dumps(item),)] for item in table_content['table'])
+        return ([(json.dumps(item),)] for item in table_content["table"])
 
     # Autentisering mot MainManager-APIet
     def _get_mainmanager_token(self):
         data = {
-            'grant_type': 'password',
-            'username': self.username,
-            'password': self.password
+            "grant_type": "password",
+            "username": self.username,
+            "password": self.password,
         }
 
         response = requests.post(urljoin(self.base_url, "/restapi/token"), data=data)
 
         if response.status_code == 200:
-            return response.json().get('access_token')
+            return response.json().get("access_token")
         else:
             print(f"Token request failed: {response.status_code}, {response.text}")
             return None
