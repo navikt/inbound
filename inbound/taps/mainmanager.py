543eb783455c5959731a6635c81514ff39c507d7
trineno <161321609+trineno@users.noreply.github.com>
2024-08-15 10:24:06 +0200

    Skille ut IO mot MainManager til MainManagerDataSupplier
    For enklere å kunne unitteste konvertering av data fra MainManager

--- inbound/taps/mainmanager.py
+++ inbound/taps/mainmanager.py
@@ -7,9 +7,7 @@ import requests
 from ..core.models import Description
 from ..sdk.tap import Tap
 
-
-class MainManagerTap(Tap):
-
+class MainManagerDataSupplier():
     def __init__(
         self,
         table: str,
@@ -23,28 +21,13 @@ class MainManagerTap(Tap):
         self.base_url = base_url
         self.endpoint = f"/api/v1/datawarehouseview?viewname={table}&datatableoutput=4)"
 
-    def column_descriptions(self) -> list[Description]:
-        return [
-            Description(
-                name="raw", type="variant", precision=None, scale=None, nullable=True
-            )
-        ]
-
-    # Henter ut json-data fra APIet og deler opp i én record per rad
-    def data_generator(self) -> Generator[list[tuple], Any, None]:
-        token = self._get_mainmanager_token()
+    def get_data(self):
+        token = self._get_mainmanager_token() # IO
         headers = {"Authorization": f"Bearer {token}"}
 
-        response = requests.get(urljoin(self.base_url, self.endpoint), headers=headers)
+        response = requests.get(urljoin(self.base_url, self.endpoint), headers=headers) # IO
 
-        eiendomsdata = response.json()
-
-        # TODO: vurdere om informasjon om antall rader skal være med til snowflake
-        # print('Antall records hentet fra MainManager-API: ', str(eiendomsdata['TotalNumberOfRecords']))
-
-        table_content = json.loads(eiendomsdata.get("DataTable"))
-
-        return ([(json.dumps(item),)] for item in table_content["table"])
+        return response.json()
 
     # Autentisering mot MainManager-APIet
     def _get_mainmanager_token(self):
@@ -54,10 +37,46 @@ class MainManagerTap(Tap):
             "password": self.password,
         }
 
-        response = requests.post(urljoin(self.base_url, "/restapi/token"), data=data)
+        response = requests.post(urljoin(self.base_url, "/restapi/token"), data=data) # IO
 
         if response.status_code == 200:
             return response.json().get("access_token")
         else:
             print(f"Token request failed: {response.status_code}, {response.text}")
             return None
+
+class MainManagerTap(Tap):
+
+    def __init__(
+        self,
+        table: str,
+        username: str,
+        password: str,
+        base_url: str = "https://nav-test.mainmanager.no",
+        data_supplier = None
+    ):
+        self.data_supplier = data_supplier
+        if data_supplier is None:
+            self.data_supplier = MainManagerDataSupplier(table=table, username=username, password=password, base_url=base_url)
+
+    def column_descriptions(self) -> list[Description]:
+        return [
+            Description(
+                name="raw", type="variant", precision=None, scale=None, nullable=True
+            )
+        ]
+
+    # Henter ut json-data fra APIet og deler opp i én record per rad
+    def data_generator(self) -> Generator[list[tuple], Any, None]:
+
+        eiendomsdata = self.data_supplier.get_data() # IO
+
+        # TODO: vurdere om informasjon om antall rader skal være med til snowflake
+        # print('Antall records hentet fra MainManager-API: ', str(eiendomsdata['TotalNumberOfRecords']))
+
+        table_content = json.loads(eiendomsdata.get("DataTable"))
+
+        return ([(json.dumps(item),)] for item in table_content["table"])
+
+    
+
