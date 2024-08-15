48998d281463f2bb471f4ba8f97bee7c82d7f806
trineno <161321609+trineno@users.noreply.github.com>
2024-08-15 14:18:02 +0200

    Oppdatere version til 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="inbound",
-    version="0.1.3",
+    version="0.1.4",
     description=(
         "Pakke for å laste dataprodukter fra ulike kildesystemer til Snowflake"
     ),
