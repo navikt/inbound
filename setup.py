52e6a5fdc0a20c9be790cb6480b4d953a2c3465c
Patrick Pedersen <patrick.pedersen@nav.no>
2024-09-25 09:44:45 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="inbound",
-    version="0.1.4",
+    version="0.2.0",
     description=(
         "Pakke for å laste dataprodukter fra ulike kildesystemer til Snowflake"
     ),
