89b9fe51de26473b17f67b3f782cb1ddbd73f111
trineno <161321609+trineno@users.noreply.github.com>
2025-06-03 08:38:42 +0200

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="inbound",
-    version="0.3.3",
+    version="0.3.4",
     description=(
         "Pakke for å laste dataprodukter fra ulike kildesystemer til Snowflake"
     ),
